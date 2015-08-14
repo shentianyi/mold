104828bf7b1a084d4effdb3fbc7d174b5faef015
lzding <472339105@qq.com>
2015-08-14 16:50:08 +0800

    fix

---
+++ RailsClient/app/controllers/users_controller.rb
@@ -0,0 +1,84 @@
+class UsersController < ApplicationController
+  #load_and_authorize_resource
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+    #@users = @users.paginate(:page=>params[:page])
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, user: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params.except(:id))
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    if @user.is_sys
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'Sysatem manager can\'t be destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:nr,:user_name,:email,:tel,:password,:password_confirmation,:role_id)
+  end
+end
