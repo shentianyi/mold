80d4ae42b015679003579a66569b811b8501f9a4
Charlot <7wangsong@gmail.com>
2015-08-28 12:44:20 +0800

    add bat and reg

---
+++ WinClient/MoldClientCon/MoldClientCon/publish.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+Set root=C:\Brilliantech\
+Set mold=C:\Brilliantech\Mold\
+
+If exist C:\Brilliantech\(
+
+)Else(
+  md "%root%"
+)
+
+If  exist "%mold%"(
+ 
+)Else(
+  md "%mold%"
+)
+
+copy * "%mold%"
+ 
